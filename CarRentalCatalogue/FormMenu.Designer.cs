243344c5acf3707765945b12b4509dad9961aac2
Gigson Aquino <gigsonaquino4@gmail.com>
2025-11-28 19:53:12 +0800

    updated with panels and buttons

--- CarRentalCatalogue/FormMenu.Designer.cs
+++ CarRentalCatalogue/FormMenu.Designer.cs
@@ -28,16 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMenu));
             panel1 = new Panel();
+            btnProduct = new Button();
+            btnOrders = new Button();
+            button2 = new Button();
+            btnNotification = new Button();
+            btnSetting = new Button();
             panelLogo = new Panel();
-            button1 = new Button();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(51, 51, 76);
-            panel1.Controls.Add(button1);
+            panel1.Controls.Add(btnSetting);
+            panel1.Controls.Add(btnNotification);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(btnOrders);
+            panel1.Controls.Add(btnProduct);
             panel1.Controls.Add(panelLogo);
             panel1.Dock = DockStyle.Left;
             panel1.Location = new Point(0, 0);
@@ -45,26 +54,109 @@
             panel1.Size = new Size(215, 541);
             panel1.TabIndex = 0;
             // 
+            // btnProduct
+            // 
+            btnProduct.BackColor = Color.FromArgb(51, 51, 76);
+            btnProduct.BackgroundImageLayout = ImageLayout.None;
+            btnProduct.Dock = DockStyle.Top;
+            btnProduct.FlatAppearance.BorderSize = 0;
+            btnProduct.FlatStyle = FlatStyle.Flat;
+            btnProduct.ForeColor = SystemColors.Control;
+            btnProduct.Image = (Image)resources.GetObject("btnProduct.Image");
+            btnProduct.ImageAlign = ContentAlignment.MiddleLeft;
+            btnProduct.Location = new Point(0, 75);
+            btnProduct.Name = "btnProduct";
+            btnProduct.Padding = new Padding(12, 0, 0, 0);
+            btnProduct.Size = new Size(215, 60);
+            btnProduct.TabIndex = 1;
+            btnProduct.Text = "           Product";
+            btnProduct.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnProduct.UseVisualStyleBackColor = false;
+            // 
+            // btnOrders
+            // 
+            btnOrders.BackColor = Color.FromArgb(51, 51, 76);
+            btnOrders.BackgroundImageLayout = ImageLayout.None;
+            btnOrders.Dock = DockStyle.Top;
+            btnOrders.FlatAppearance.BorderSize = 0;
+            btnOrders.FlatStyle = FlatStyle.Flat;
+            btnOrders.ForeColor = SystemColors.Control;
+            btnOrders.Image = (Image)resources.GetObject("btnOrders.Image");
+            btnOrders.ImageAlign = ContentAlignment.MiddleLeft;
+            btnOrders.Location = new Point(0, 135);
+            btnOrders.Name = "btnOrders";
+            btnOrders.Padding = new Padding(12, 0, 0, 0);
+            btnOrders.Size = new Size(215, 60);
+            btnOrders.TabIndex = 2;
+            btnOrders.Text = "           Orders";
+            btnOrders.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnOrders.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(51, 51, 76);
+            button2.BackgroundImageLayout = ImageLayout.None;
+            button2.Dock = DockStyle.Top;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = SystemColors.Control;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(0, 195);
+            button2.Name = "button2";
+            button2.Padding = new Padding(12, 0, 0, 0);
+            button2.Size = new Size(215, 60);
+            button2.TabIndex = 3;
+            button2.Text = "           Costumer Info";
+            button2.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // btnNotification
+            // 
+            btnNotification.BackColor = Color.FromArgb(51, 51, 76);
+            btnNotification.BackgroundImageLayout = ImageLayout.None;
+            btnNotification.Dock = DockStyle.Top;
+            btnNotification.FlatAppearance.BorderSize = 0;
+            btnNotification.FlatStyle = FlatStyle.Flat;
+            btnNotification.ForeColor = SystemColors.Control;
+            btnNotification.Image = (Image)resources.GetObject("btnNotification.Image");
+            btnNotification.ImageAlign = ContentAlignment.MiddleLeft;
+            btnNotification.Location = new Point(0, 255);
+            btnNotification.Name = "btnNotification";
+            btnNotification.Padding = new Padding(12, 0, 0, 0);
+            btnNotification.Size = new Size(215, 60);
+            btnNotification.TabIndex = 5;
+            btnNotification.Text = "           Notification";
+            btnNotification.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnNotification.UseVisualStyleBackColor = false;
+            // 
+            // btnSetting
+            // 
+            btnSetting.BackColor = Color.FromArgb(51, 51, 76);
+            btnSetting.BackgroundImageLayout = ImageLayout.None;
+            btnSetting.Dock = DockStyle.Top;
+            btnSetting.FlatAppearance.BorderSize = 0;
+            btnSetting.FlatStyle = FlatStyle.Flat;
+            btnSetting.ForeColor = SystemColors.Control;
+            btnSetting.Image = (Image)resources.GetObject("btnSetting.Image");
+            btnSetting.ImageAlign = ContentAlignment.MiddleLeft;
+            btnSetting.Location = new Point(0, 315);
+            btnSetting.Name = "btnSetting";
+            btnSetting.Padding = new Padding(12, 0, 0, 0);
+            btnSetting.Size = new Size(215, 60);
+            btnSetting.TabIndex = 6;
+            btnSetting.Text = "           Setting";
+            btnSetting.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnSetting.UseVisualStyleBackColor = false;
+            // 
             // panelLogo
             // 
+            panelLogo.BackColor = Color.FromArgb(52, 51, 76);
             panelLogo.Dock = DockStyle.Top;
             panelLogo.Location = new Point(0, 0);
             panelLogo.Name = "panelLogo";
             panelLogo.Size = new Size(215, 75);
             panelLogo.TabIndex = 0;
-            // 
-            // button1
-            // 
-            button1.BackColor = Color.White;
-            button1.Dock = DockStyle.Top;
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Location = new Point(0, 75);
-            button1.Name = "button1";
-            button1.Size = new Size(215, 60);
-            button1.TabIndex = 1;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = false;
             // 
             // FormMenu
             // 
@@ -83,7 +175,11 @@
         #endregion
 
         private Panel panel1;
+        private Button btnProduct;
+        private Button btnSetting;
+        private Button btnNotification;
+        private Button button2;
+        private Button btnOrders;
         private Panel panelLogo;
-        private Button button1;
     }
 }
